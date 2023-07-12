d4cd52ba07f2901dfe9f6345794ba872a8940ef9
Andrew Wen <Andrew.Wen@uth.tmc.edu>
2023-07-12 14:13:44 -0500

    Initial Test-Ready Version

--- presidio_deid.py
+++ presidio_deid.py
@@ -1,16 +1,21 @@
 import json
 from typing import Union, List
 
-from ohnlp.toolkit.backbone.api import BackboneComponentDefinition, BackboneComponent, BackboneComponentOneToOneDoFn, \
-    Row, Schema, SchemaField, FieldType, TypeName, BackboneComponentOneToManyDoFn, TaggedRow
+from ohnlp.toolkit.backbone.api import BackboneComponentDefinition, BackboneComponent, Row, Schema, SchemaField, \
+    FieldType, BackboneComponentOneToManyDoFn, TaggedRow, BackboneComponentOneToOneDoFn
 from presidio_analyzer import AnalyzerEngine
 from presidio_analyzer.nlp_engine import NlpEngineProvider
 from presidio_anonymizer import AnonymizerEngine
 from presidio_anonymizer.entities import RecognizerResult
-
 # Huggingface/Transformers and Spacy Model Support
 from transformers import AutoTokenizer, AutoModelForTokenClassification
-from spacy.cli import download
+
+
+def resolve_from_json_config(config, field: str):
+    if field in config:
+        return config[field]
+    else:
+        return None
 
 
 class IdentifyPIIComponent(BackboneComponent):
@@ -20,11 +25,14 @@ class IdentifyPIIComponent(BackboneComponent):
         self.note_id_column_name = None
         self.input_col = None
 
-    def init(self, configstr: str) -> None:
-        self.config = configstr
-        configobj = json.loads(self.config)
-        self.note_id_column_name = configobj["id_col"]
-        self.input_col = configobj["text_col"]
+    def init(self, configstr: Union[str, None]) -> None:
+        if configstr is not None:
+            self.config = configstr
+            configobj = json.loads(self.config)
+            self.note_id_column_name = resolve_from_json_config(configobj, 'id_col')
+            if self.note_id_column_name is not None:
+                self.note_id_column_name = self.note_id_column_name["sourceColumnName"]
+            self.input_col = resolve_from_json_config(configobj, 'text_col')
 
     def to_do_fn_config(self) -> str:
         return self.config
@@ -35,17 +43,25 @@ class IdentifyPIIComponent(BackboneComponent):
     def get_output_tags(self) -> List[str]:
         return ["PII Positions", "Text w/ PII Redacted"]
 
-    def calculate_output_schema(self, input_schema: Schema) -> dict[str, Schema]:
-        pii_entity_schema: Schema = Schema([
-            SchemaField(self.note_id_column_name, input_schema.get_field(self.note_id_column_name).field_type),
-            SchemaField("entity_type", FieldType(TypeName.STRING)),
-            SchemaField("start_offset", FieldType(TypeName.INT32)),
-            SchemaField("end_offset", FieldType(TypeName.INT32)),
-            SchemaField("confidence_score", FieldType(TypeName.DOUBLE))
-        ])
+    def calculate_output_schema(self, input_schema: dict[str, Schema]) -> dict[str, Schema]:
+        pii_entity_schema: Union[Schema, None] = None
+        deid_text_schema: Union[Schema, None] = None
+        for key in input_schema:
+            schema = input_schema[key]
+            pii_entity_schema = Schema([
+                SchemaField(
+                    self.note_id_column_name,
+                    schema.get_field(self.note_id_column_name).get_field_type()
+                ),
+                SchemaField("entity_type", FieldType("STRING")),
+                SchemaField("start_offset", FieldType("INT32")),
+                SchemaField("end_offset", FieldType("INT32")),
+                SchemaField("confidence_score", FieldType("DOUBLE"))
+            ], self.gateway)
+            deid_text_schema = schema
         return {
             "PII Positions": pii_entity_schema,
-            "De-Identified Text": input_schema
+            "Text w/ PII Redacted": deid_text_schema
         }
 
 
@@ -60,25 +76,25 @@ class IdentifyPIIDoFn(BackboneComponentOneToManyDoFn):
         self.pii_entity_schema = None
         self.transformers_model = None
 
-    def init_from_driver(self, config_json_str: str) -> None:
-        config = json.loads(config_json_str)
-        self.note_id_col_name = config['id_col']
-        self.note_text_col_name = config['text_col']
-        self.transformers_model = config['ner_model_path']
-        self.acceptance_threshold = config['acceptance_threshold']
+    def init_from_driver(self, config_json_str: Union[str, None]) -> None:
+        if config_json_str is not None:
+            config = json.loads(config_json_str)
+            self.note_id_col_name = resolve_from_json_config(config, 'id_col')
+            self.note_text_col_name = resolve_from_json_config(config, 'text_col')
+            self.transformers_model = resolve_from_json_config(config, 'ner_model_path')
+            self.acceptance_threshold = resolve_from_json_config(config, 'acceptance_threshold')
 
     def on_bundle_start(self) -> None:
         # Download models
         AutoTokenizer.from_pretrained(self.transformers_model)
         AutoModelForTokenClassification.from_pretrained(self.transformers_model)
-        download("en_core_web_lg")  # TODO offline mode
         presidio_analyzer_configuration = {
             "nlp_engine_name": "transformers",
             "models": [
                 {
                     "lang_code": "en",
                     "model_name": {
-                        "spacy": "en_core_web_lg",
+                        "spacy": "en_core_web_lg",  # Installed as part of
                         "transformers": self.transformers_model
                     }
                 }
@@ -96,12 +112,12 @@ class IdentifyPIIDoFn(BackboneComponentOneToManyDoFn):
     def apply(self, row: Row) -> List[TaggedRow]:
         if self.pii_entity_schema is None:
             self.pii_entity_schema: Schema = Schema([
-                SchemaField(self.note_id_col_name, row.get_schema().get_field(self.note_id_col_name).field_type),
-                SchemaField("entity_type", FieldType(TypeName.STRING)),
-                SchemaField("start_offset", FieldType(TypeName.INT32)),
-                SchemaField("end_offset", FieldType(TypeName.INT32)),
-                SchemaField("confidence_score", FieldType(TypeName.DOUBLE))
-            ])
+                SchemaField(self.note_id_col_name, row.get_schema().get_field(self.note_id_col_name).get_field_type()),
+                SchemaField("entity_type", FieldType("STRING")),
+                SchemaField("start_offset", FieldType("INT32")),
+                SchemaField("end_offset", FieldType("INT32")),
+                SchemaField("confidence_score", FieldType("DOUBLE"))
+            ], self.gateway)
 
         ret: List[TaggedRow] = []
         input_text: str = str(row.get_value(self.note_text_col_name))
@@ -139,3 +155,11 @@ class IdentifyPIIComponentDefinition(BackboneComponentDefinition):
 
     def get_do_fn(self) -> BackboneComponentOneToManyDoFn:
         return IdentifyPIIDoFn()
+
+
+class ReplacePIIComponentDefinition(BackboneComponentDefinition):
+    def get_component_def(self) -> BackboneComponent:
+        pass
+
+    def get_do_fn(self) -> Union[BackboneComponentOneToOneDoFn, BackboneComponentOneToManyDoFn]:
+        pass
